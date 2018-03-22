1c126747a8c680c19849f7080f148fd479a6ce6a
Maryam <mkarimif@uwo.ca>
2018-03-22 19:59:12 -0400

    Login

--- cs2212_Project/src/cs2212_Project/Login.java
+++ cs2212_Project/src/cs2212_Project/Login.java
@@ -1,8 +1,21 @@
 package cs2212_Project;
 
+import java.util.Scanner;
+
 public class Login {
+
+	LoginServer server;
+	Scanner input;
 	
-	public Login(String username, String password) {
+	public Login() {
+		server = new LoginServer("userpass.txt");
+	}
+	
+	public void signIn(String username,String password) {
+		
+		server.isValid(username, password);
+
+		
 		
 	}
 	

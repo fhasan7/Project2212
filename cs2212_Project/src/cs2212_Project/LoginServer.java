cc430c11c1f54df4721b185d8c192981e9da0835
Maryam <mkarimif@uwo.ca>
2018-03-22 15:28:40 -0400

    LoginServer

---
+++ cs2212_Project/src/cs2212_Project/LoginServer.java
@@ -0,0 +1,14 @@
+
+public class LoginServer {
+
+	private final String fileName = "userpass.txt";
+	private Hashtable<String,String> hashtable;
+	
+	public LoginServer(){
+		
+	}
+	
+	public boolean isValid(String username, String password) {
+		
+	}
+}
